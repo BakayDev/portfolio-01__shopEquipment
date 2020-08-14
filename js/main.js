628df1a051a342d47b5c9e4046b3fb7811b32f8b
andreySavchenko <58919772+sava911@users.noreply.github.com>
2020-08-14 18:55:24 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow: '<button><img class="banner-arrow banner-arrow_prev" src="images/content/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button><img class="banner-arrow banner-arrow__nex" src="images/content/arrow-right.svg" alt=""></button>'
+    });
+
+
+
+});
