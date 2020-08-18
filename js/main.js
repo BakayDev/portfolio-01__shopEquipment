c20867ace23c9c43753c83e2413a6480e21153c4
andreySavchenko <58919772+sava911@users.noreply.github.com>
2020-08-19 00:35:43 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,6 +6,24 @@ $(function () {
         nextArrow: '<button><img class="banner-arrow banner-arrow__nex" src="images/content/arrow-right.svg" alt=""></button>'
     });
 
+    $('.tab').on('click', function (e) {
+        e.preventDefault();
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).parent().siblings().find('div')).removeClass('tabs-contant--active');
 
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-contant--active')
+
+    })
+
+    $('.products-item__favorite').on('click', function name(params) {
+        $('.products-item__favorite').toggleClass('products-item__favorite--active')
+    })
+    $('.products__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button><img class="banner-arrow banner-arrow_prev" src="images/arrow-black-left.svg" alt=""></button>',
+        nextArrow: '<button><img class="banner-arrow banner-arrow__nex" src="images/arrow-black-right.svg" alt=""></button>',
+    });
 
 });
