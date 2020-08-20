d71b3d3b8315fd7cca253cb92b5c89366dc4da22
andreySavchenko <58919772+sava911@users.noreply.github.com>
2020-08-21 01:17:53 +0300

    section tabs

--- js/main.js
+++ js/main.js
@@ -1,29 +1,32 @@
 $(function () {
+  $(".banner-section__slider").slick({
+    dots: true,
+    prevArrow:
+      '<button><img class="banner-arrow banner-arrow__prev" src="images/content/arrow-left.svg" alt=""></button>',
+    nextArrow:
+      '<button><img class="banner-arrow banner-arrow__nex" src="images/content/arrow-right.svg" alt=""></button>',
+  });
 
-    $('.banner-section__slider').slick({
-        dots: true,
-        prevArrow: '<button><img class="banner-arrow banner-arrow_prev" src="images/content/arrow-left.svg" alt=""></button>',
-        nextArrow: '<button><img class="banner-arrow banner-arrow__nex" src="images/content/arrow-right.svg" alt=""></button>'
-    });
+  $(".tab").on("click", function (e) {
+    e.preventDefault();
+    $($(this).siblings()).removeClass("tab--active");
+    $($(this).parent().siblings().find("div")).removeClass(
+      "tabs-contant--active"
+    );
 
-    $('.tab').on('click', function (e) {
-        e.preventDefault();
-        $($(this).siblings()).removeClass('tab--active');
-        $($(this).parent().siblings().find('div')).removeClass('tabs-contant--active');
+    $(this).addClass("tab--active");
+    $($(this).attr("href")).addClass("tabs-contant--active");
+  });
 
-        $(this).addClass('tab--active');
-        $($(this).attr('href')).addClass('tabs-contant--active')
-
-    })
-
-    $('.products-item__favorite').on('click', function name(params) {
-        $('.products-item__favorite').toggleClass('products-item__favorite--active')
-    })
-    $('.products__slider').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        prevArrow: '<button><img class="banner-arrow banner-arrow_prev" src="images/arrow-black-left.svg" alt=""></button>',
-        nextArrow: '<button><img class="banner-arrow banner-arrow__nex" src="images/arrow-black-right.svg" alt=""></button>',
-    });
-
+  $(".products-item__favorite").on("click", function name(params) {
+    $(this).toggleClass("products-item__favorite--active");
+  });
+  $(".products__slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:
+      '<button><img class="products__slider-arrow products__slider-arrow__prev" src="images/arrow-black-left.svg" alt=""></button>',
+    nextArrow:
+      '<button><img class="products__slider-arrow products__slider-arrow__nex" src="images/arrow-black-right.svg" alt=""></button>',
+  });
 });
